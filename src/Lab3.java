98809635095c7f5ee4147ee7451f7b255176ebca
Adriana Cente <adrianacente12@gmail.com>
2020-09-07 19:59:43 +0300

    first commit

---
+++ src/Lab3.java
@@ -0,0 +1,10 @@
+public class Lab3 {
+    public static void main(String[] args) {
+
+//        Given a number, if it’s equal to or higher than 2 and equal to or lower than 8, print the
+//        number
+        LogicalOperations logic = new LogicalOperations();
+        System.out.println(logic.checkComparison(1));
+
+    }
+}
---
+++ src/LogicalOperations.java
@@ -0,0 +1,9 @@
+public class LogicalOperations {
+
+    public int checkComparison(int number) {
+        if (number >=2 && number <= 8) {
+            return number;
+        }
+        return 0;
+    }
+}
