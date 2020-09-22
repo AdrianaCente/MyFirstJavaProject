07a48919e9b644b30d215841b787ef3a2246464e
Adriana Cente <adrianacente12@gmail.com>
2020-09-22 15:07:54 +0300

    added array homework

---
+++ src/Lab5.java
@@ -0,0 +1,123 @@
+public class Lab5 {
+    public static void main(String[] args) {
+        LogicalOperations operation = new LogicalOperations();
+
+//        Array homework
+
+//        1. In proiectul de Calculator, in clasa Calculator, supraincarcati toate metodele
+//        conform principiului polimorfismului. Creati cel putin cate doua metode polimorfe
+//        pentru fiecare metoda pe care o aveti in clasa.
+
+//        !!!!! We haven't learn yet about polymorphism in java !!!!!
+
+//        2.  In clasa LogicalOp, creati o metoda care sa scrie pe un array de 100 de pozitii,
+//        valorile de la 1 la 100. Sa se afiseze progresul in consola pe tot parcursul.
+//        Apelati metoda in main() pentru a verifica daca functioneaza.
+        int[] returnedArray = operation.getArrayToNumber(100);
+        System.out.print("Print values from 1 to 100: ");
+        operation.printArray(returnedArray);
+        System.out.println();
+
+//        3. Creati o metoda care primeasca un parametru de tip array de numere intregi gol,
+//        si sa il returneze populat cu toate valorile pare de la 1 la 100.
+//        Apelati metoda in main() pentru a verifica daca functioneaza.
+//        Atentie, metoda returneaza un array, deci acesta va trebui surprins si afisat folosind o bucla.
+        int[] receivedArray = new int[50];
+        System.out.print("Print even values from 1 to 100: ");
+        operation.printArray(operation.evenArray(receivedArray));
+        System.out.println();
+
+//        4. Creati o metoda care sa primeasca un parametru de tip array de numere, si sa fie populat cu valori.
+//        Metoda sa calculeze si sa returneze media numerelor din array.
+//        Apelati metoda in main() pentru a verifica daca functioneaza.
+        System.out.println("The average in this array is: " + operation.computeAverage(new int[]{2, 3, 4, 5, 6,7}));
+
+//        5. Creati o metoda care sa primeasca un parametru de tip array de string-uri,
+//        populat cu valori, si un parametru de tip String. Metoda sa verifice daca valoarea string-ului primit
+//        se regaseste in array-ul primit, iar daca da sa returneze true iar daca nu, sa returneze false.
+//        Apelati metoda in main() pentru a verifica daca functioneaza.
+        String[] sentArray = new String[]{"Ana", "are", "mere", "si", "pere"};
+        System.out.println("Is the value in the array: " + operation.isValueInArray(sentArray, "ana"));
+
+//        6. Creati o metoda care sa primeasca un parametru de tip array de numere, populat cu valori, si un parametru
+//        de tip numar. Metoda sa verifice daca numarul primit se afla in array-ul primit, si daca da, atunci sa
+//        returneze pozitia pe care se afla numarul.  Apelati metoda in main() pentru a verifica daca functioneaza.
+        int[] sentArrayOfNumbers = new int[]{2, 8, 9, 1, 77, 6, 44};
+        String result = (operation.isNumberInArray(sentArrayOfNumbers, 44) == -1) ? "The number is not in the array." : String.format("The number " +
+                "is in the array at position %s.", operation.isNumberInArray(sentArrayOfNumbers, 44));
+        System.out.println(result);
+
+//        7. Creati o metoda care sa afiseze urmatoarea grila, folosind un array:
+        System.out.println("Print pattern: ");
+        operation.printLinePattern(10);
+
+//        8. Creati o metoda care sa primeasca un parametru de tip array de numere, populat cu valori, si un parametru de tip numar.
+//        Metoda sa verifice daca numarul exista in array, si daca da, sa returneze array-ul primit, fara acel numar.
+//        Apelati metoda in main() pentru a verifica daca functioneaza.
+        int[] initialArrayOfNumbers = new int[] {1, 2, 8, 9, 77, 41, 7, 53, 7};
+        System.out.print("The array without the specified value is: ");
+        operation.printArray(operation.arrayWithoutReceivedValue(initialArrayOfNumbers, 8));
+        System.out.println();
+
+//        9. Creati o metoda care sa primeasca un array, si sa returneze al doilea cel mai mic numar din array.
+//        Apelati metoda in main() pentru a verifica daca functioneaza.
+        int[] sentNumberArray = new int[]{20, 89, 77, 39, 48, 45, 61, 1, 33};
+        System.out.println("The second min in the array is: " + operation.secondMinInArray(sentNumberArray));
+        //second method
+        System.out.println("The second min in the array is: " + operation.secondMinInArraySecond(sentNumberArray));
+
+//    10. Creati o metoda care sa primeasca doi parametrii: un array populat cu valori,
+//    si un array gol. Metoda sa copieze toate valorile din primul array, parcurgandu-l, in cel de-al doilea.
+//    Apelati metoda in main() pentru a verifica daca functioneaza.
+        int[] sentArrayNumbers = new int[]{7, 26, 33, 1, 79, 5, 3, 1, 56};
+        int[] receivedArrayOfNumbers = new int[sentArrayNumbers.length];
+        System.out.print("The copied array is: ");
+        operation.printArray(operation.copiedArray(sentArrayNumbers, receivedArrayOfNumbers));
+        System.out.println();
+
+//    Array optional homework
+
+//    1. Creati o metoda care sa returneze al doilea cel mai mic numar dintr-un array.
+//    !!!!! The exercise is solved at number 9.!!!!!
+
+//    2. Creati o metoda care insereze un element pe o pozitie specifica intr-un array.
+        int[] sentArrayNumbersTwo = new int[]{7, 26, 33, 1, 79, 5, 3, 1, 56};
+        System.out.print("The array with the inserted value is: ");
+        operation.printArray(operation.insertElementInArray(sentArrayNumbersTwo, 1, 100));
+        System.out.println();
+
+//    3. Creati o metoda care sa gaseasca cel mai mare si cel mai mic numar dintr-un array.
+        int[] minMaxArray = new int[]{7, 26, 33, 1, 79, 5, 3, 1, 56};
+        operation.findMaxMinArray(minMaxArray);
+
+//        4. Creati o metoda care sa inverseze valorile unui array de int-uri.
+        int[] sentArrayRevers = new int[]{7, 26, 33, 1, 79, 5, 3, 1, 56};
+        System.out.print("The reversed array is: ");
+        operation.printArray(operation.reverseArray(sentArrayRevers));
+        System.out.println();
+//        second method
+        int[] sentArrayReversTwo = new int[]{75, 22, 3, 78, 5, 6, 1, 3, 74, 3};
+        System.out.print("The reversed array is: ");
+        operation.printArray(operation.reverseArrayTwo(sentArrayReversTwo));
+        System.out.println();
+
+//      5. Creati o metoda care sa gaseasca toate valorile duplicate dintr-un array.
+        int[] duplicateArray = new int[]{75, 22, 3, 75, 5, 6, 1, 3, 74, 99};
+        System.out.println("The duplicate elements are: ");
+        operation.findDuplicateValues(duplicateArray);
+
+//       6. Creati o metoda care sa gaseasca toate elementele comune intre doua array-uri (array de String).
+        String[] firstArray = new String[] {"Ana", "are", "mere", "si", "pere"};
+        String[] secondArray = new String[] {"Maria", "are", "caise", "sau", "pere"};
+        System.out.println("The common elements are: ");
+        operation.findCommonElementsInArrays(firstArray, secondArray);
+        System.out.println();
+
+//       7. Creati o metoda care sa primeasca un array de numere ne-ordonat,
+//        si sa il returneze ordonat crescator.
+        int[] sentNumbersArray = new int[]{100, 3, 5, 20, 89, 77, 39, 48, 45, 61, 1, 33};
+        System.out.print("The sorted array: ");
+        operation.printArray(operation.sortArray(sentNumbersArray));
+        System.out.println();
+    }
+}
--- src/LogicalOperations.java
+++ src/LogicalOperations.java
@@ -3,7 +3,7 @@ import com.sun.source.tree.WhileLoopTree;
 public class LogicalOperations {
 
     public int checkBiggerNumber(int firstNumber, int secondNumber) {
-        if ( firstNumber > secondNumber) {
+        if (firstNumber > secondNumber) {
             return firstNumber;
         } else {
             return secondNumber;
@@ -18,10 +18,10 @@ public class LogicalOperations {
         }
     }
 
-    public String checkTextAndNumber(String input, int number){
+    public String checkTextAndNumber(String input, int number) {
         if (input.equals("FastTrack") && number <= 3) {
             return (input + number);
-        } else if (!input.equals("FastTrack") && number >= 4){
+        } else if (!input.equals("FastTrack") && number >= 4) {
             return (number + input);
         }
         return "";
@@ -33,6 +33,7 @@ public class LogicalOperations {
         }
         return "The forecast snow is(cm): " + number;
     }
+
     public String checkNumber(int number) {
         if (number > 3 && number != 4) {
             return "The number is greater than 3 and not equal to 4";
@@ -46,17 +47,37 @@ public class LogicalOperations {
 
     public void pressedKeyWithoutConcatenation(int number) {
         switch (number) {
-            case 9: System.out.println("The number is: 9!"); break;
-            case 8: System.out.println("The number is: 8!"); break;
-            case 7: System.out.println("The number is: 7!"); break;
-            case 6: System.out.println("The number is: 6!"); break;
-            case 5: System.out.println("The number is: 5!"); break;
-            case 4: System.out.println("The number is: 4!"); break;
-            case 3: System.out.println("The number is: 3!"); break;
-            case 2: System.out.println("The number is: 2!"); break;
-            case 1: System.out.println("The number is: 1!"); break;
-            case 0: System.out.println("The number is: 0!"); break;
-        //  default: System.out.println("Not allowed");
+            case 9:
+                System.out.println("The number is: 9!");
+                break;
+            case 8:
+                System.out.println("The number is: 8!");
+                break;
+            case 7:
+                System.out.println("The number is: 7!");
+                break;
+            case 6:
+                System.out.println("The number is: 6!");
+                break;
+            case 5:
+                System.out.println("The number is: 5!");
+                break;
+            case 4:
+                System.out.println("The number is: 4!");
+                break;
+            case 3:
+                System.out.println("The number is: 3!");
+                break;
+            case 2:
+                System.out.println("The number is: 2!");
+                break;
+            case 1:
+                System.out.println("The number is: 1!");
+                break;
+            case 0:
+                System.out.println("The number is: 0!");
+                break;
+            //  default: System.out.println("Not allowed");
         }
     }
 
@@ -97,7 +118,7 @@ public class LogicalOperations {
 
     // Exercises from class
     public int checkComparison(int number) {
-        if (number >=2 && number <= 8) {
+        if (number >= 2 && number <= 8) {
             return number;
         }
         return 0;
@@ -115,8 +136,10 @@ public class LogicalOperations {
             case 3:
             case 2:
             case 1:
-                System.out.println(number); break;
-            default: System.out.println("Not allowed");
+                System.out.println(number);
+                break;
+            default:
+                System.out.println("Not allowed");
         }
     }
 
@@ -205,9 +228,9 @@ public class LogicalOperations {
         float count = 0;
         for (int i = startNumber; i <= 100; i++) {
             sum += i;
-            count ++;
+            count++;
         }
-        return (sum/count);
+        return (sum / count);
     }
 
     public void printPattern() {
@@ -297,10 +320,10 @@ public class LogicalOperations {
         while (i <= 8899) {
             sum += i;
             i++;
-            count ++;
+            count++;
         }
         System.out.println("The sum of the numbers between 111 and 8899 is: " + sum);
-        System.out.println("The average of the numbers between 111 and 8899 is: " + sum/count);
+        System.out.println("The average of the numbers between 111 and 8899 is: " + sum / count);
     }
 
     public void printAverageDivisibilityWithSeven(int startValue, int endValue) {
@@ -314,7 +337,7 @@ public class LogicalOperations {
             }
             i++;
         }
-        System.out.println("The average of the numbers divisible by seven is: " + sum/count);
+        System.out.println("The average of the numbers divisible by seven is: " + sum / count);
     }
 
     public void printFibonacci() {
@@ -386,4 +409,200 @@ public class LogicalOperations {
             i++;
         }
     }
+
+
+//    Array
+
+    public int[] getArrayToNumber(int maxValue) {
+        int[] myArray = new int[maxValue];
+        for (int i = 0; i < maxValue; i++) {
+            myArray[i] = i + 1;
+        }
+        return myArray;
+    }
+
+    public void printArray(int[] myArray) {
+        for (int i : myArray) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public int[] evenArray(int[] resultedArray) {
+        int[] wholeArray = getArrayToNumber(100);
+        int j = 0;
+        for (int i = 0; i < wholeArray.length; i++) {
+            if (wholeArray[i] % 2 == 0) {
+                resultedArray[j] = wholeArray[i];
+                j++;
+            }
+        }
+        return resultedArray;
+    }
+
+    public void printLinePattern(int receivedValue) {
+        String[] myArray = new String[receivedValue];
+        for (int j = 0; j < receivedValue; j++) {
+            for (int i = 0; i < receivedValue; i++) {
+                myArray[i] = "-";
+                System.out.print(myArray[i]);
+            }
+            System.out.println();
+        }
+    }
+
+    public float computeAverage(int[] secondArray) {
+        int sum = 0;
+        for (int i = 0; i < secondArray.length; i++) {
+            sum += secondArray[i];
+        }
+        return sum / secondArray.length;
+    }
+
+    public boolean isValueInArray(String[] receivedArray, String valueToCheck) {
+        for (int i = 0; i < receivedArray.length; i++) {
+            if (receivedArray[i].toLowerCase().equals(valueToCheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int isNumberInArray(int[] receivedArray, int valueToCheck) {
+        for (int i = 0; i < receivedArray.length; i++) {
+            if (receivedArray[i] == valueToCheck) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int[] arrayWithoutReceivedValue(int[] receivedArray, int valueToCheck) {
+        int[] resultedArray = new int[receivedArray.length - 1];
+        int j = 0;
+        try {
+            for (int i = 0; i < receivedArray.length; i++) {
+                if (receivedArray[i] == valueToCheck) {
+                    continue;
+                }
+                resultedArray[j] = receivedArray[i];
+                j++;
+            }
+            return resultedArray;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return receivedArray;
+        }
+    }
+
+    public int minValueInArray(int[] receivedArray) {
+        int min = receivedArray[0];
+        for (int i = 0; i < receivedArray.length; i++) {
+            if (receivedArray[i] < min) {
+                min = receivedArray[i];
+            }
+        }
+        return min;
+    }
+
+    public int secondMinInArray(int[] receivedArray) {
+        int firstMin = minValueInArray(receivedArray);
+        int[] arrayWithoutFirstMin = arrayWithoutReceivedValue(receivedArray, firstMin);
+        int secondMin = minValueInArray(arrayWithoutFirstMin);
+        return secondMin;
+    }
+
+    public int[] sortArray(int[] receivedArray) {
+        int temp = 0;
+        for (int i = 0; i < receivedArray.length; i++) {
+            for (int j = i + 1; j < receivedArray.length; j++)
+                if (receivedArray[i] > receivedArray[j]) {
+                    temp = receivedArray[i];
+                    receivedArray[i] = receivedArray[j];
+                    receivedArray[j] = temp;
+                }
+        }
+        return receivedArray;
+    }
+
+    public int secondMinInArraySecond(int[] receivedArray) {
+        int[] sortedArray = sortArray(receivedArray);
+        return sortedArray[1];
+    }
+
+    public int[] copiedArray(int[] receivedArray, int[] resultedArray) {
+        int j = 0;
+        for (int i = 0; i < receivedArray.length; i++) {
+            resultedArray[j] = receivedArray[i];
+            j++;
+        }
+        return resultedArray;
+    }
+
+    public int[] insertElementInArray(int[] receivedArray, int sentIndex, int newValue) {
+        for (int i = receivedArray.length - 1; i > sentIndex; i--) {
+            receivedArray[i] = receivedArray[i - 1];
+        }
+        receivedArray[sentIndex] = newValue;
+        return receivedArray;
+    }
+
+    public void findMaxMinArray(int[] receivedArray) {
+        int max = receivedArray[0];
+        int min = receivedArray[0];
+        for (int i = 0; i < receivedArray.length; i++) {
+            if (receivedArray[i] > max) {
+                max = receivedArray[i];
+            }
+            if (receivedArray[i] < min) {
+                min = receivedArray[i];
+            }
+        }
+        System.out.println("Max from the array is: " + max + ". Min from the array is: " + min + ".");
+    }
+
+    public int[] reverseArray(int[] receivedArray) {
+        int[] resultedArray = new int[receivedArray.length];
+        int i = 0;
+        for (int j = receivedArray.length - 1; j >= 0; j--) {
+            resultedArray[i] = receivedArray[j];
+            i++;
+        }
+        return resultedArray;
+    }
+
+    public int[] reverseArrayTwo(int[] receivedArray) {
+        int temp = 0;
+        for (int j = 0; j <= receivedArray.length / 2; j++) {
+            temp = receivedArray[j];
+            receivedArray[j] = receivedArray[receivedArray.length - j - 1];
+            receivedArray[receivedArray.length - j - 1] = temp;
+        }
+        return receivedArray;
+    }
+
+    public void findDuplicateValues(int[] receivedArray) {
+        int count = 0;
+        for (int i = 0; i < receivedArray.length; i++) {
+            for (int j = i + 1; j < receivedArray.length; j++) {
+                if (receivedArray[i] == receivedArray[j]){
+                    count++;
+                    break;
+                }
+            }
+            if (count == 1) {
+                System.out.println(receivedArray[i]);
+                count = 0;
+            }
+        }
+    }
+
+    public void findCommonElementsInArrays(String[] firstArray, String[] secondArray) {
+        for (int i =0; i < firstArray.length; i++) {
+            for (int j = 0; j < secondArray.length; j++) {
+                if (firstArray[i].equals(secondArray[j])) {
+                    System.out.println(firstArray[i] + " ");
+                }
+            }
+        }
+    }
+
 }
